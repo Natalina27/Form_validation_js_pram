fc7b5508d9c84465444caec464b80b87071caba3
Natalya Myunster <natalyamyunster@yahoo.com>
2020-08-11 10:56:06 +0200

    проверка если поля не заполнены

--- script.js
+++ script.js
@@ -11,19 +11,17 @@ const fields = form.querySelectorAll('.field');
 //нужно повесить евент submit на нашу форму
 form.addEventListener('submit', function (event) {
     event.preventDefault(); //запрещает поведение по умолчанию (html по умолчанию отправляет форму и перезагружает при этом страницу)
-    console.log('clicked on validate');
-    console.log('username: ', username.value);
-    console.log('password: ', password.value);
-    console.log('passwordConfirmation: ', passwordConfirmation.value);
-    console.log('where: ', where.value);
-    console.log('message: ', message.value);
-});
-
-//проверим, что все поля у нас заполнены и если поля не заполнены выведем их в консоль
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+    //проверим, что все поля у нас заполнены и если поля не заполнены выведем их в консоль
     fields.forEach(item =>  {
         !item.value && console.log ('field is blank', item);
     });
+
+    //выведем  ошибки на форму
+    //сгенерируем новый элемент и добавим к каждому полю, которое не заполнено.
+    //Добавим еще красный цвет и текст Cannot be blank.
+    //чтобы вставить его перед нашими полями используем insertBefore.
+    // А так как нам нужно указать парента, то используем .parentElement.
+
+
 });
+
