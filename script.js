915b1aa15d8419ba812f42e17748e791e7f06b6f
Natalya Myunster <natalyamyunster@yahoo.com>
2020-08-12 10:09:16 +0200

    выводим красную запись если поле не заполнено

--- script.js
+++ script.js
@@ -10,18 +10,43 @@ const fields = form.querySelectorAll('.field');
 
 //нужно повесить евент submit на нашу форму
 form.addEventListener('submit', function (event) {
+
     event.preventDefault(); //запрещает поведение по умолчанию (html по умолчанию отправляет форму и перезагружает при этом страницу)
+
+
+    //Но если мы нажмем еще раз validate, то все наши сообщения сдублируются.
+// Самый простой способ этого избежать, это удалять все ошибки с страницы при валидации.
+
+
+
+
+
+
+
+
+
+
+
+
     //проверим, что все поля у нас заполнены и если поля не заполнены выведем их в консоль
-    fields.forEach(item =>  {
-        !item.value && console.log ('field is blank', item);
-    });
+    fields.forEach(item => {
+        if(!item.value) {
+            console.log('field is blank', item);
+            //выведем  ошибки на форму
+            //сгенерируем новый элемент и добавим к каждому полю, которое не заполнено.
+            const error = document.createElement('div');
+            error.className = 'error';
+            //Добавим еще красный цвет и текст Cannot be blank.
+            error.style.color = 'red';
+            error.innerHTML = ' Cannot be blank';
+            //чтобы вставить его перед нашими полями используем insertBefore.
+            // А так как нам нужно указать парента, то используем .parentElement.
+            item.parentElement.insertBefore(error, item);
+        }
+    })
+})
 
-    //выведем  ошибки на форму
-    //сгенерируем новый элемент и добавим к каждому полю, которое не заполнено.
-    //Добавим еще красный цвет и текст Cannot be blank.
-    //чтобы вставить его перед нашими полями используем insertBefore.
-    // А так как нам нужно указать парента, то используем .parentElement.
 
 
-});
+
 
